f76d1b90cbc3431a06ed4d47ae2e65214b3eeb50
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-08 15:46:54 +0500

    Create basic server structure code with injectied .env variables

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+
+
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './config/config.env' });
+
+const PORT = process.dotenv.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
