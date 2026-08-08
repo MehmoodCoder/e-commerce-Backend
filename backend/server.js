232e56f2998beda9f767f7fcf0c67dc412a88e94
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-08 22:09:13 +0500

    Also format code

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import app from "./app.js";
 
+import dotenv from "dotenv";
 
-import dotenv from 'dotenv'
-
-dotenv.config({ path: 'backend/config/config.env' });
+dotenv.config({ path: "backend/config/config.env" });
 
 const PORT = process.dotenv.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+  console.log(`Server started on port ${PORT}`);
+});
