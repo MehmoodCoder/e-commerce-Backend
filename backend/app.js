628428518d6beafb883398baf863a775d2b126a8
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 15:50:06 +0500

    Import Error handler MiddleWare and make it middleware

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,16 @@ import express from "express";
 
 const app = express();
 
+import ErrorHanderlerMiddleWare from './middlewares/error.js';
+
 import Product from "./routes/productRoute.js";
 
 app.use(express.json());
 
 app.use("/api/v1", Product);
+
+// Middleware
+
+app.use(ErrorHanderlerMiddleWare)
 
 export default app;
