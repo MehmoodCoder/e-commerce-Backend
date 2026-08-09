a69e998c838baa14408b2e27315bb5ae86c40080
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 11:45:15 +0500

    resolve error in injection

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "backend/config/config.env" });
 
-const PORT = process.dotenv.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
