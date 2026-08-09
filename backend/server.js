fb29e288c46680c77ba49f8eb3bdc35e94c22cc2
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 12:18:20 +0500

    Pass URL of DB from .env file with then.catch chaining

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import app from "./app.js";
 
+import ConnectDB from "./connectDB.js";
+
 import dotenv from "dotenv";
 
 dotenv.config({ path: "backend/config/config.env" });
 
 const PORT = process.env.PORT || 5000;
+
+ConnectDB(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((error) => {
+    console.error("Database connection failure:", error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
