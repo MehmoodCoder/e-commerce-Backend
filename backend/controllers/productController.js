87126ed8db3c6ef44e8fdadd087e1a854bd087ea
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 19:11:29 +0500

    Import functionalities from ApiFeatures i.e., search, filter & pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 import Product from "../models/productModel.js";
 import ErrorHandler from "../utils/Errorhandler.js";
 import catchAsyncErrors from "../middlewares/asyncErrorHandler.js";
+import ApiFeatures from "../utils/ApiFeatures.js";
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -12,11 +13,20 @@ export const createProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 export const getAllProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find({});
+  const resultPerPage = 20;
+  const ProductCount = await Product.countDocuments();
+
+  const apiFeature = new ApiFeatures(Product.find({}), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+
+  const products = await apiFeature.query;
 
   res.status(200).json({
     success: true,
     data: products,
+    count: ProductCount,
   });
 });
 
