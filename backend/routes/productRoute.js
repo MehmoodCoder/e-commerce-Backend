22f00cd87a2839be88f9a6d5b64770e924f2c8b2
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 15:29:44 +0500

    Add get route for ProductDetails

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createProduct, getAllProducts, UpdateProduct, DeleteProduct } from "../controllers/productController.js";
+import { createProduct, getAllProducts, UpdateProduct, DeleteProduct, ProductDetails } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route('/products/create').post(createProduct)
-router.route('/products/:id').put(UpdateProduct).delete(DeleteProduct)
+router.route('/products/:id').put(UpdateProduct).delete(DeleteProduct).get(ProductDetails)
 
 export default router;
