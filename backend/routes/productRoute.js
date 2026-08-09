9915fb00ab1a337bc1a22840a81d1815c55be78d
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 14:14:11 +0500

    Add update route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createProduct, getAllProducts } from "../controllers/productController.js";
+import { createProduct, getAllProducts, UpdateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route('/products/create').post(createProduct)
+router.route('/products/:id').put(UpdateProduct)
 
 export default router;
