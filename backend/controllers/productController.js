178248a6693dae78c852697b4b7494cfe6a96c42
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 13:39:28 +0500

    Add functionality of Create & add the exact functionality of get route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,34 @@
-export async function getAllProducts(req, res) {
-  // will add exact functionality here
-  res.status(200).json({ message: "Working" });
+import ProductSchema from "../models/productModel.js";
+
+// Only for Admin 
+export const createProduct = async (req, res) => {
+  try {
+    const product = await ProductSchema.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await ProductSchema.find({})
+
+    res.status(201).json({
+      success: true,
+      data: products,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
