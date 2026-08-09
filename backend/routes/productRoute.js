394dd02ec540a6f61cbebe65d6493403573b906a
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 13:37:54 +0500

    Add /create route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllProducts } from "../controllers/productController.js";
+import { createProduct, getAllProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+router.route('/products/create').post(createProduct)
 
 export default router;
