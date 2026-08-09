da51e428d40a0b88621b30cc439e1be752d585a4
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 16:26:26 +0500

    Add security and also add message in response

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -4,8 +4,14 @@ export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
-  res.statusCode(err.statusCode).json({
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
     success: false,
+    message: err.message,
     error: err.stack,
   });
 };
