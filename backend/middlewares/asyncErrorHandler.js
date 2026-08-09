7431d37fb124b9956436c2c13fa2fc18f42de6bf
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-09 17:36:04 +0500

    Rename parameter

--- backend/middlewares/asyncErrorHandler.js
+++ backend/middlewares/asyncErrorHandler.js
@@ -1,3 +1,3 @@
-export default (Security) => (req, res, next) => {
-  Promise.resolve(Security(req, res, next)).catch(next);
+export default (theFunc) => (req, res, next) => {
+  Promise.resolve(theFunc(req, res, next)).catch(next);
 };
