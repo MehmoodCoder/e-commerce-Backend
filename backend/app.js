9b83fcd101f31ae436f3db5dcc2ea9265e4db1ca
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-10 18:59:19 +0500

    Format code

--- backend/app.js
+++ backend/app.js
@@ -3,20 +3,19 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-import ErrorHanderlerMiddleWare from './middlewares/error.js';
+import ErrorHanderlerMiddleWare from "./middlewares/error.js";
 
 import ProductRoutes from "./routes/productRoute.js";
-import UserRoutes from './routes/userRoute.js'
+import UserRoutes from "./routes/userRoute.js";
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/v1", ProductRoutes);
 app.use("/api/v1", UserRoutes);
 
-
 // Middleware
 
-app.use(ErrorHanderlerMiddleWare)
+app.use(ErrorHanderlerMiddleWare);
 
 export default app;
