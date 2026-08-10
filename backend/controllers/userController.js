1542fe0a1c98c7bb84417d50623c1688d47bb680
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-10 19:21:31 +0500

    Add logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,3 +39,16 @@ export const loginUser = asyncErrorHandler(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+export const logout = asyncErrorHandler( async (req, res, next) => {
+
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logout successfully"
+    })
+})
