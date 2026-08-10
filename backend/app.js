91ca6e7260957968d874b9fffb5c53990a0295fd
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-10 18:58:40 +0500

    import cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -8,6 +9,7 @@ import ProductRoutes from "./routes/productRoute.js";
 import UserRoutes from './routes/userRoute.js'
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/v1", ProductRoutes);
 app.use("/api/v1", UserRoutes);
