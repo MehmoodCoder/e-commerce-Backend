bb28886c69f52280e4387d02617aed7ed68ca992
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-10 19:09:35 +0500

    Add isAuthenticatedUser to secure owr app

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,35 @@
+import ErrorHandler from "../utils/Errorhandler.js";
+import asyncErrorHandler from "./asyncErrorHandler.js";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const isAuthenticatedUser = asyncErrorHandler(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHandler("Please Login to access it", 401));
+  }
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
+
+// export const authorizeRoles = (...roles) => {
+//   return (req, res, next) => {
+//     if (!roles.includes(req.user.role)) {
+//       return next(
+//         new ErrorHandler(
+//           `Role: ${req.user.role} is not allowed to access this resouce `,
+//           403
+//         )
+//       );
+//     }
+
+//     next();
+//   };
+// };
