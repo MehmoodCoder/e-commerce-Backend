87a075eee194c8a94351b31a15bfc424dd189f7c
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-10 08:57:09 +0500

    Write functionality of User Route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+import ErrorHandler from "../utils/Errorhandler.js";
+import asyncErrorHandler from "../middlewares/asyncErrorHandler.js";
+import User from "../models/userModel.js";
+
+export const RegisterUser = asyncErrorHandler( async (req, res, next) => {
+    const {name, email, password} = req.body
+    const user = await User.create({
+        name, email, password,
+        avatar: {
+            public_id: "This is public id",
+            url: "profileImage"
+        }
+    })
+
+    res.status(201).json({
+        success: true,
+        data: user
+    })
+})
