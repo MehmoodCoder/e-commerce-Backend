493a1fc635d87697e02aeff04421a84976d30c62
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-10 19:20:59 +0500

    create /logout route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { loginUser, RegisterUser } from '../controllers/userController.js'
+import { loginUser, logout, RegisterUser } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.route('/register').post(RegisterUser)
 router.route('/login').post(loginUser)
+router.route("/logout").get(logout)
 
 export default router
