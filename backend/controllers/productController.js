28f9ff6c10230832b2d4611fe63699032508f12e
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-10 20:46:15 +0500

    Manually get admin role user to verify which admin of application create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,6 +4,9 @@ import catchAsyncErrors from "../middlewares/asyncErrorHandler.js";
 import ApiFeatures from "../utils/ApiFeatures.js";
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
+
+  req.body.user = req.user.id
+
   const product = await Product.create(req.body);
 
   res.status(201).json({
