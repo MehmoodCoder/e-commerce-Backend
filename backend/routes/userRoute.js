69fe92bbbc06f6189b8847d73454f3635c5ef137
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 08:47:57 +0500

    Format code

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,18 @@
-import express from 'express'
-import { forgotPassword, loginUser, logout, RegisterUser, resetPassword } from '../controllers/userController.js'
+import express from "express";
+import {
+  forgotPassword,
+  loginUser,
+  logout,
+  RegisterUser,
+  resetPassword,
+} from "../controllers/userController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/register').post(RegisterUser)
-router.route('/login').post(loginUser)
-router.route('/password/forgot').post(forgotPassword)
-router.route('/password/reset/:token').put(resetPassword)
-router.route("/logout").get(logout)
+router.route("/register").post(RegisterUser);
+router.route("/login").post(loginUser);
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+router.route("/logout").get(logout);
 
-export default router
+export default router;
