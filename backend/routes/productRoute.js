6ad403f716714ac8390df2087775b72c35e1e673
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 19:43:24 +0500

    Separate the product details route because normal user can use this route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -11,11 +11,14 @@ import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/admin/products/create").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/admin/products/create")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 router
   .route("/admin/products/:id")
   .put(isAuthenticatedUser, authorizeRoles("admin"), UpdateProduct)
-  .delete(isAuthenticatedUser, authorizeRoles("admin"), DeleteProduct)
-  .get(ProductDetails);
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), DeleteProduct);
+
+router.route("/products/:id").get(ProductDetails);
 
 export default router;
