ef78e58eb3ac9a103d3ae1001a905ee5ae0785e5
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 09:10:52 +0500

    Add error handler for dublicate email error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -9,6 +9,11 @@ export default (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
