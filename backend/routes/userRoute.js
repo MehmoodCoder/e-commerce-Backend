b63da198618490e50aa0256e5ab2e623cc3ef9ff
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 08:47:39 +0500

    Create reset password route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { forgotPassword, loginUser, logout, RegisterUser } from '../controllers/userController.js'
+import { forgotPassword, loginUser, logout, RegisterUser, resetPassword } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.route('/register').post(RegisterUser)
 router.route('/login').post(loginUser)
 router.route('/password/forgot').post(forgotPassword)
+router.route('/password/reset/:token').put(resetPassword)
 router.route("/logout").get(logout)
 
 export default router
