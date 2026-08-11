eb836813f79d1031e1f305b191fdda91a8a8bebe
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 09:14:19 +0500

    Format code

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -14,7 +14,7 @@ export default (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
-   if (err.name === "JsonWebTokenError") {
+  if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again `;
     err = new ErrorHandler(message, 400);
   }
