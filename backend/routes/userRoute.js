321a42aac9f1dd7b38ee28c1374527344b7c8882
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 20:17:55 +0500

    Format code

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -24,8 +24,11 @@ router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
-router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser)
-router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
-
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser);
 
 export default router;
