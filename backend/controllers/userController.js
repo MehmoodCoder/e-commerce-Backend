a828d2889a5646d4d9b32f9bb3029dd02eee0e22
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 20:04:40 +0500

    Add functionality of get all users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -170,3 +170,13 @@ export const updateProfile = asyncErrorHandler(async (req, res, next) => {
   });
 });
 
+export const getAllUser = asyncErrorHandler(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+
