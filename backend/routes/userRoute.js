12f15a4c92dab86086b8a8d3426f4737d1aaf520
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 15:06:41 +0500

    Add get route for userDetails page

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
 import {
   forgotPassword,
+  getUserDetails,
   loginUser,
   logout,
   RegisterUser,
   resetPassword,
 } from "../controllers/userController.js";
+
+import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -14,5 +17,7 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+
+router.route('/me').get(isAuthenticatedUser, getUserDetails)
 
 export default router;
