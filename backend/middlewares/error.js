a1ed23c9fcad21c63b80327620d50ed44de8a0df
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 09:12:49 +0500

    Add error handler for wrong jwt token error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -14,6 +14,11 @@ export default (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+   if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
