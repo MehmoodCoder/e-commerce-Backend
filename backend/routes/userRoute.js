612b08160a5b5c288ffc057f246a26af5a56bc34
MehmoodCoder <mehmood.mh56@gmail.com>
2026-08-11 16:15:04 +0500

    Format code

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -17,7 +17,6 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
-
-router.route('/me').get(isAuthenticatedUser, getUserDetails)
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 export default router;
